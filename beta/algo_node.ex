43ecc247a622210d2aac60a3f72b14dcd7a96ac2
FlorEtcheverry <florencia.a.etcheverry@gmail.com>
2017-02-05 23:17:05 +0100

    changed master to infinite loop menu

--- beta/algo_node.ex
+++ beta/algo_node.ex
@@ -1,12 +1,25 @@
 defmodule AlgorithmNode do
   require Logger
 
-  def start(next) do
+  def start do
+    IO.puts "process created and running."
     receive do
-      {:start_algorithm, from} ->
+      {:start_algorithm, from, next} ->
         Logger.info "Received command from #{from} to start algorithm. My next is #{next}."
         IO.puts "started"
-        start(next)
+        start_(next)
+      {:end_algorithm, from} ->
+        IO.puts "finish"
+        Logger.info "Received command from #{from} to end algorithm."
+    end
+  end
+
+  def start_(next) do
+    IO.puts "process created and running."
+    receive do
+      {:start_algorithm, from, next} ->
+        Logger.info "Algorithm already started."
+        IO.puts "Algorithm already started."
       {:token, from} ->
         Logger.info "Received token from #{from}."
         :timer.sleep(1000)
@@ -14,10 +27,11 @@ defmodule AlgorithmNode do
         IO.puts "sending token"
         send(next,{:token})
         Logger.info "Sent token to #{next}."
-        start(next)
+        start_(next)
       {:end_algorithm, from} ->
         IO.puts "finish"
         Logger.info "Received command from #{from} to end algorithm."
     end
   end
+
 end
--- beta/master.ex
+++ beta/master.ex
@@ -1,23 +1,39 @@
 defmodule Master do
 
-  @list [:"foo@florencia-VirtualBox", :"dos@florencia-VirtualBox", :"tres@florencia-VirtualBox"]
-  def setup do
-    Enum.with_index(@list)
-    |> Enum.each(fn {x,i} ->
-      IO.puts "sending start/1"
-        pid = Node.spawn_link(x,AlgorithmNode,:start,[Enum.at(@list,rem(i+1,length(@list)))])
-        IO.puts(inspect(pid))
+  def start do
+    nodes = Map.new([:"foo@florencia-VirtualBox", :"dos@florencia-VirtualBox", :"tres@florencia-VirtualBox"],fn x-> {x,0} end)
+    run_loop(nodes)
+  end
+
+  defp run_loop(nodes) do
+    nodes = case IO.gets("Insert next command (setup, start, end):\n") do
+      "setup\n" -> setup_nodes(nodes)
+      "start\n" -> start_algo(nodes)
+      "end\n" -> end_algo(nodes)
+    end
+    IO.inspect(nodes)
+    run_loop(nodes)
+  end
+
+  defp setup_nodes(nodes) do
+    names = Map.keys(nodes)
+    n_collec = Enum.map(names, fn x ->
+        {x ,Node.spawn_link(x,AlgorithmNode,:start,[])}
       end)
+      Map.new(n_collec)
   end
 
-  def start_algo do
-    Enum.each(@list, fn x -> send(x,{:start_algorithm,Node.self()}) end)
-    send(:"foo@florencia-VirtualBox",{:token,Node.self()})
+  defp start_algo(nodes) do
+    names = Map.keys(nodes)
+    Enum.each(Enum.with_index(names), fn {x,i} -> send(Map.get(nodes,x),{:start_algorithm,Node.self(),Enum.at(names,rem(i+1,length(names)))}) end)
+    send( Map.get(nodes,List.first(Map.keys(nodes))), {:token,Node.self()} )
+    nodes
   end
 
-  def end_algo do
-    Enum.each(@list, fn x ->
-      send(x,{:end_algorithm,self()})
+  defp end_algo(nodes) do
+    Enum.each(Map.keys(nodes), fn x ->
+      send(Map.get(nodes,x),{:end_algorithm,Node.self()})
     end)
+    nodes
   end
 end
